1ab46c405706ca569db3a514eb46782c2cd85090
coder-us-mj <coder.us.jm@gmail.com>
2025-02-10 21:26:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+
+
+
+
+// ......................hamburger................
+ const hambar = document.querySelector('.hambar')
+ const nav = document.querySelector('.ul_class')
+
+ hambar.addEventListener('click',()=>{
+    hambar.classList.toggle('baar')
+    nav.classList.toggle('nav_list')
+    
+
+})
+
+{/* <section class="">
+        <div class="testimonial">
+            <div class="swiper mySwiper max-h-lg max-w-lg border-2 border-black flex  flex-col gap-10 items-center text-center ">
+                <div class="flex flex-col gap-4 mt-10">
+                    <h4 class="text-xl text-[#D1631C] font-medium">TESTIMONIALS</h4>
+                    <h3 class="text-3xl font-bold">What Our Clients Say</h3>
+                    <p class="text-sm text-[#494B4F]">Our top priority is customer satisfaction, and we work closely with clients to understand their
+                        unique needs and goals.</p>
+                </div>
+                <div class="swiper-wrapper  ">
+                    <div class="swiper-slide testimonial flex flex-col items-center bg-[#fbf3ea] gap-5 px-10 py-10 ">
+                        <div class="w-16 h-16 rounded-full border-2 ">
+                            <img  class="rounded-full" src="./image/businessman-enterprize-urban-occupation-man-concept-300x300.jpg" alt="">
+                        </div>
+                        <p>
+                            Fusce dapibus tellus ac cursus commodo tortor mauris condimentum nibh ut fermentum massa
+                            justo sit amet risus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+                            Etiam porta sem malesuada magna mollis euismod.
+                        </p>
+                        <div>
+                            <h4 class="text-[#D1631C]">LUKE COLMAN</h4>
+                            <h5 class="text-[#494B4F]">Director, Sterich PLC</h5>
+                        </div>
+                    </div>
+                      <div class="swiper-slide testimonial flex flex-col items-center bg-[#fbf3ea] gap-5 px-10 py-10 ">
+                        <div class="w-16 h-16 rounded-full border-2 ">
+                            <img  class="rounded-full" src="./image/businessman-enterprize-urban-occupation-man-concept-300x300.jpg" alt="">
+                        </div>
+                        <p>
+                            Fusce dapibus tellus ac cursus commodo tortor mauris condimentum nibh ut fermentum massa
+                            justo sit amet risus. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+                            Etiam porta sem malesuada magna mollis euismod.
+                        </p>
+                        <div>
+                            <h4 class="text-[#D1631C]">LUKE COLMAN</h4>
+                            <h5 class="text-[#494B4F]">Director, Sterich PLC</h5>
+                        </div>
+                    </div>
+
+
+                </div>
+                <div class="swiper-pagination"></div>
+            </div>
+        </div>
+    </section> */}
